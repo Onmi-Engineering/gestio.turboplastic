25dfae17ede670f3a3e835671e813e6abbc49b4b
vcarbonell-onmi <vcarbonell@onmi.es>
2025-10-28 10:17:18 +0100

    [SO11][ADD] Ocultar la referencia interna de los productos en las líneas de las facturas de venta

---
+++ onmi_kingfa_customs/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'KINGFA Customizations',
+    'version': '18.0.0.1',
+    'summary': 'KINGFA Customizations',
+    'description': 'KINGFA Customizations',
+    'category':  'ONMI developments',
+    'author': 'ONMI Engineering',
+    'license': 'LGPL-3',
+    'depends': ['account'],
+    'data': [
+        'views/report_invoice.xml'
+        ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
