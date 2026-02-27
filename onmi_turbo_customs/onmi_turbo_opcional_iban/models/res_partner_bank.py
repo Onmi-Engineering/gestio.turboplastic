bf1fce5651f59e48885ab71a3222cf0a904adc5e
vcarbonell-onmi <vcarbonell@onmi.es>
2026-02-27 09:16:23 +0100

    [#SO109][ADD] Corrección de la dirección del banco

--- onmi_turbo_customs/onmi_turbo_opcional_iban/models/res_partner_bank.py
+++ onmi_turbo_customs/onmi_turbo_opcional_iban/models/res_partner_bank.py
@@ -27,7 +27,7 @@ class ResPartnerBank(models.Model):
             if rec.bank_bic:
                 lines.append(f"BIC: {rec.bank_bic}")
             if rec.bank_id.street:
-                lines.append(f"ADDRESS: {rec.partner_id.street}, {rec.partner_id.street2 or ''}, {rec.partner_id.zip or ''}")
+                lines.append(f"ADDRESS: {rec.bank_id.street}, {rec.bank_id.street2 or ''}, {rec.bank_id.zip or ''}")
             if rec.bank_id.city and rec.bank_id.country.name:
-                lines.append(f"{rec.partner_id.city}, {rec.partner_id.country_id.name}")
+                lines.append(f"{rec.bank_id.city}, {rec.bank_id.country.name}")
             rec.display_text = '\n'.join(lines)
